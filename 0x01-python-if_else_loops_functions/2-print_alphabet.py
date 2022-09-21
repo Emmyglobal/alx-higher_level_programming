29ac0a816e6b13accb2e295b793f92664dc341a4
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-09-21 01:55:16 -0700

    fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
+"""print the alphabet in lowercase, not followed by a new line"""
 for letters in range(97,123):
-    print(f"{chr(letters)}", end = " ")
+    print(f"{chr(letters)}", end = "")
