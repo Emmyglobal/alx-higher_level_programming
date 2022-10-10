1603f432d3eee7a0ee4ffe9b05a340a32cfac276
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-10 09:18:15 -0700

    hhh

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for x in my_list:
-            print("{}".format(x), end="")
-    except:
+            print("{}".format(my_list[x]), end="")
+    except (ValueError, TypeError):
         return(x)
--- 0x05-python-exceptions/1-main.py
+++ 0x05-python-exceptions/1-main.py
@@ -15,3 +15,14 @@ value = "School"
 has_been_print = safe_print_integer(value)
 if not has_been_print:
     print("{} is not an integer".format(value))
+
+safe_print_list = __import__('0-safe_print_list').safe_print_list
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
