d143ef3f49fd7caf181ac2478fb0025bb7d7e943
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-10 07:01:14 -0700

    first task

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-safe_print_list = __import__('0-safe_print_list').safe_print_list
 
-my_list = [1, 2, 3, 4, 5]
+# A function that prints x elements of a list
+# x : The number of elements to print
 
-nb_print = safe_print_list(my_list, 2)
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list))
-print("nb_print: {:d}".format(nb_print))
-nb_print = safe_print_list(my_list, len(my_list) + 2)
-print("nb_print: {:d}".format(nb_print))
+def safe_print_list(my_list=[], x=0):
+    try:
+        for x in my_list:
+            print("{}".format(x), end="")
+    except:
+        return(x)
