35c875fc4ab1eca4c4f7d0836cd2c2cfc73248cb
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-10 07:27:48 -0700

    first task

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,6 +3,7 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
     except:
+        print("{} is not an integer".format(value))
         return(False)
     else:
         return(True)
