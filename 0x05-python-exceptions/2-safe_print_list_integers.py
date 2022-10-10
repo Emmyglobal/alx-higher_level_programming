f380ced1daf195eee9d4a687688deba050ac8096
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-10 16:09:14 -0700

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# a function that prints the first x elements of a list and only integers
+# x: input value and a new list to be printed
+
+def safe_print_list_integers(my_list=[], x=0):
+    n = 0
+    for j in range(x):
+        try:
+            print("{:d}".format(my_list[j]), end="")
+        except (IndexError, TypeError):
+            n += 1
+    print()
+    return n
