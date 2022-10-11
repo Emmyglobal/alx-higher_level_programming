36f9bac1b5306199a6374580f755d37c8dce9099
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-10 22:09:26 -0700

    task 9

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -2,9 +2,11 @@
 def magic_calculation(a, b):
     add = a + b
     sub = a - b
-    if a < b:
-        c = add(a, b)
-        for i in range(4, 6):
-            c = add(c, i)
+    try:
+        if a < b:
+            c = add(a, b)
+            for i in range(a, b):
+                c = add(c, i)
         return c
-    return sub(a, b)
+    except (TypeError, ValueError):
+        return sub(a, b)
