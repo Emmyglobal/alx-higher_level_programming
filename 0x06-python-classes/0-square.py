b20b3b97e195c781712346b317b0967a849bc361
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-12 21:40:23 -0700

    task0 to 6

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+A ppytjon program to demonstrate class
+"""
+
+
+class Square:
+    """
+    An empty class called square
+    """
+    pass
+Footer
+© 2022 GitHub, Inc.
---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+A python file to demonstrate class and object
+"""
+
+
+class Square:
+    """
+    A python class that has a value
+    """
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return (self.__size)**2
