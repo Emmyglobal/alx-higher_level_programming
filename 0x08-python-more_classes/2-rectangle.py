8f67640e3e0650f677268592f15740103d29bf3f
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-10-17 22:09:23 -0700

    fixed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,23 +1,45 @@
 #!/usr/bin/python3
+'''
+a python class
+'''
+
+
 class Rectangle:
-        def __init__(self, width=0, height=0):
-                self.width = width
-                self.height = height
-        def width(self, value):
-                if type(self.width) is not int:
-                        raise TypeError
-                elif self.width < 0:
-                        raise ValueError
-                else:
-                        return self.width
-        def height(self, value):
-                if type(self.height) is not int:
-                        raise TypeError
-                elif self.height < 0:
-                        raise ValueError
-	def area(self,value):
-		self.area = self.width * self.height
-		return self.area
-	def perimeter(self, value):
-		self.perimeter = 2*(self.height + self.width)
-		return self.perimeter
+    '''
+    that has value
+    '''
+
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(self.width) is not int:
+            raise TypeError
+        elif self.width < 0:
+            raise ValueError
+        else:
+            return self.width
+
+    @property
+    def height(self)
+	return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(self.height) is not int:
+            raise TypeError
+        elif self.height < 0:
+            raise ValueError
+    def area(self,value):
+        self.area = self.width * self.height
+        return self.area
+    def perimeter(self, value):
+        self.perimeter = 2*(self.height + self.width)
+	return self.perimeter
