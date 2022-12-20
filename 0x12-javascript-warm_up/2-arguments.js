b17fdedd601a97c9b525ecf014b0cb942ec150d4
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-12-20 00:16:39 -0800

    task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let argument;
+
+if (argument == null){
+	console.log('No argument');
+}
+else if(argument == 1){
+	console.log('Argument found');
+}
+else{
+	console.log('Arguments found');
+}
