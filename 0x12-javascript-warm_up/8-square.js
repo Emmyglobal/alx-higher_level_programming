835b2a679c2e1ada959eb06af4713f90952a8fc5
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-12-20 09:30:50 -0800

    task 8 done

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
+
 if (parseInt(process.argv[2])) {
   for (let i = 1; i <= parseInt(process.argv[2]); i++) {
-    console.log('C is fun');
+    console.log('X'.repeat(parseInt(process.argv[2])));
   }
 } else {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 }
