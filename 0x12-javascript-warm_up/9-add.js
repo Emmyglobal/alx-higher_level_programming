be4c04b30883d990af49aa91c775fd8d05092f81
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-12-20 09:56:49 -0800

    task 8

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,5 +5,5 @@ const sum = parseInt(process.argv[2]) + parseInt(process.argv[3]);
 if (parseInt(process.argv[2]) && parseInt(process.argv[3])) {
   console.log(sum);
 } else {
-	console.log('NaN');
+  console.log('NaN');
 }
