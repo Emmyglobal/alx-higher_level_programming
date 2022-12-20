c64f3d7f1a990b1cd06e4e58d3b5f814a8900d8e
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-12-20 02:46:27 -0800

    hot fix

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,17 +1,14 @@
 #!/usr/bin/node
 
 if (process.argv[3]) {
-<<<<<<< HEAD
-  console.log('Arguments found');
+console.log('Arguments found');
 } else if (process.argv[2]) {
-  console.log('Argument found');
+console.log('Argument found');
 } else {
-  console.log('No argument');
-=======
-	console.log('Arguments found');
+console.log('No argument');
+console.log('Arguments found');
 } else if (process.argv[2]) {
-	console.log('Argument found');
+console.log('Argument found');
 } else {
-	console.log('No argument');
->>>>>>> f45a565df1bd438441b68ac80cb54482e5916a01
+console.log('No argument');
 }
