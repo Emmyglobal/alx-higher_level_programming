f45a565df1bd438441b68ac80cb54482e5916a01
Emmyglobal <nwaforugochukwu21@gmail.com>
2022-12-20 00:35:07 -0800

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,8 @@
 
 if (process.argv[3]) {
 	console.log('Arguments found');
-}
-else if (process.argv[2]) {
+} else if (process.argv[2]) {
 	console.log('Argument found');
-}
-else {
+} else {
 	console.log('No argument');
 }
