24bbbf50e0a6be953700b150f9c4bc07c31c8cd2
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-01-01 03:32:56 -0800

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -3,7 +3,7 @@
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
 
-  for (let i = 0; i < list.lenght; i++) {
+  for (let i = 0; i < list.length; i++) {
     count = (list[i] === searchElement ? count + 1 : count);
   }
 
