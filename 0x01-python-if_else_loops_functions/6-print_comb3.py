2709be35ab6ef7fa9ff985a1bbf8e0726ad4c75f
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-10 16:59:15 -0700

    print combination

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for x in range(0, 10):
-    for y in range(x + 1, 10):
-        if x == 8 and y == 9:
-            print('89')
-        else:
-            print('{}{}, '.format(x, y), end='')
+for i in range(0, 10):
+    for j in range(1, 10):
+        if i < j and i != 8:
+            print("{}{}".format(i, j), end=", ")
+        elif i < j:
+            print("{}{}".format(i, j))
