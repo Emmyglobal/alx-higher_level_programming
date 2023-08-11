ace5c5480099da6efe1ff757616b5510eebf738f
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-10 22:52:06 -0700

    insert number into

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * insert_node -> Inserts a number into a sorted singly list
+ * Description: A number is inserted into a sorted list
+ * @head: The firs node
+ * @number: number to be inserted
+ * Return: Address of new node or NULL if failed
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
