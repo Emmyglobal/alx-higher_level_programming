a0fde46d016743d09f7ac6d7965e84336ef2d3c0
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-10 18:24:21 -0700

    uppercasew

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-''' prints a string in uppercase followed by a new line '''
-
 def uppercase(str):
-    for i in str:
-        num = ord(i)
-        if num >= 97 and num < 124:
-            num = num - 32
-        print("{}".format(chr(num)), end='')
-    print("\n")
+    for letter in str:
+        let = ord(letter)
+        if let >= 97 and let < 124:
+            let = let - 32
+        print("{}".format(chr(let)), end='')
+    print()
