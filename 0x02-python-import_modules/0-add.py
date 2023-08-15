727ea7afaf54a63ebb8623a5488e82790c0907f4
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-15 03:19:08 -0700

    All good

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3#!/usr/bin/python3
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from add_0 import add
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    import add_o
+    print("1 + 2 = {}".format(add_o.add(1, 2)))
