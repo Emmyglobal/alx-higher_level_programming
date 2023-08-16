a9a52ced4f5910c6615e5c25d393bc95804f7b9e
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-16 16:00:40 -0700

    done

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+if __name__ == '__main__':
     import hidden_4
-    for word in dir(hidden_4):
-        if not(word[0] == '_' and word[1] == '_'):
-            print("{}".format(word))
+    for i in dir(hidden_4):
+        if i[0] != '_':
+            print(i)
