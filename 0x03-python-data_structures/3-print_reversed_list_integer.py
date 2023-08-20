e9df992de29500a9fcd612760a8727daaf5eb4b9
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-19 20:08:06 -0700

    prints in reverse order

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' Prints all element in reverse order '''
+
+
+def print_reversed_list_integer(my_list=[]):
+    for i in range(len(my_list) - 1, -1, -1):
+        print("{:d}".format(my_list[i]))
