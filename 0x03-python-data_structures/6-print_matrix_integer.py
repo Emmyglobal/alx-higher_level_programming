4948b0dceeada5f94a7b7dfcb20f6e79639616f4
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-26 16:04:12 -0700

    2 by 2 considered

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,9 @@
 def print_matrix_integer(matrix=[[]]):
     if matrix:
         for row in matrix:
-            if row:
+            if row and row[2]:
                 print("{:d} {:d} {:d}".format(row[0], row[1], row[2]))
+            elif row and row[1]:
+                print("{:d} {:d} {:d}".format(row[0], row[1]))
             else:
                 print()
