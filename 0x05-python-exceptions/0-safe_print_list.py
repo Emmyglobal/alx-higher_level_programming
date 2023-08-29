00e508a18955a05c1bc7d02b9343dad3f9878412
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-28 20:39:34 -0700

    prints list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''  prints x element of a list'''
+
+
+
+def safe_print_list(my_list=[], x=0):
+    counter = 0
+    for element in my_list:
+        try:
+            if counter < x:
+                print("{}".format(element), end='')
+                counter += 1
+        except IndexError:
+            break
+    print()
+    return counter
---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+''' prints an integer with "{:d}".format().'''
+
+
+
+def safe_print_integer(value):
+
