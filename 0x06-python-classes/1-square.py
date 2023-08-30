b69a5f885a188ecaff2f5d89563b1520f9e9f642
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-29 21:27:43 -0700

    no attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """Defines a class: Square with a private attribute: size"""
+    def __init__(self, size):
+        self.__size = size
