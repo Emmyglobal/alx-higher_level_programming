22d62873d21df0d28fb24e332e985c192357e28b
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-08-29 21:51:55 -0700

    raises error

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""defines a square"""
+
+
+class Square:
+    """Defines a class: Square with a private attribute: size and raises exception"""
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            self.__size = size
+        else:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
