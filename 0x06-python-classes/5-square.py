d7548d440b8c9f2210882332dc12a98dcbdc5aa4
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-04 05:52:18 -0700

    Hot fixed

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -3,14 +3,25 @@
 
 
 class Square:
-    """Defines a class: Square with a public attribute:
-    size and raises exceptions and returns the area of the square @property and setter
+    """Defines a class: Square with a private attribute:
+    size and raises exceptions and returns the area of
+    the square @property and setter
     """
     def __init__(self, size=0):
         self.size = size
 
     def area(self):
         return pow(self.__size, 2)
+
+    def my_print(self):
+        if self.__size == 0:
+            return print()
+        else:
+            for i in range(pow(self.__size, 2)):
+                if (i % self.__size == 0) and (i != 0):
+                    print("\n")
+                print("#", end="")
+        print()
 
     @property
     def size(self):
@@ -22,12 +33,5 @@ class Square:
             self.__size = size
         else:
             raise TypeError("size must be an integer")
-        if value < 0:
-            raise valueError("size must be >= 0")
-
-    def my_print(self):
-        if self.__size == 0:
-            print()
-        else:
-            for i in range(pow(slf.__size, 2)):
-                print('#', end='')
+        if size < 0:
+            raise ValueError("size must be >= 0")
