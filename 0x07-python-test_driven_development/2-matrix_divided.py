8fa8c65a65e4a720430d546d0f8a6ec2b47ba977
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-09 15:24:24 -0700

    fixed

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,23 +3,26 @@
 
 
 def matrix_divided(matrix, div):
-    """Divides all the elements of a matrix
-
-    Args:
-    """
+    """Divides all the elements of a matrix"""
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if not div:
         raise ZeroDivisionError("division by zero")
     if not isinstance(matrix, list):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of"
+                        " integers/floats")
 
     for row in matrix:
         if not isinstance(row, list):
-            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-        if len(row) !=  len(matrix[0]):
-            raise OverflowError("Each row of the matrix must have the same size")
+            raise TypeError("matrix must be a matrix (list of lists) of"
+                            " integers/floats")
+        if len(row) != len(matrix[0]):
+            raise OverflowError("Each row of the matrix must have the"
+                                " same size")
         for element in row:
             if not isinstance(element, (int, float)):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
-    return list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix.copy()))
+                raise TypeError("matrix must be a matrix (list of lists) of"
+                                " integers/floats")
+
+    return list(map(lambda x: list(map(lambda y:
+                round(y / div, 2), x)), matrix.copy()))
