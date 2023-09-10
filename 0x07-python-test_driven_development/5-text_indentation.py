9d8e1ccf6c58b1820aa5fdb4c1dcbc2f202c1d4d
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-10 13:35:02 -0700

    task 5

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,15 +1,29 @@
 #!/usr/bin/python3
-"""A function that prints a text"""
+"""
+The Function prints a text with 2 new libes after each of these
+characters: '.', '?' and ':'
+"""
 
 
 def text_indentation(text):
-    """ prints a text with 2 new lines after each of these characters: ., ? and :"""
-    if not isinstance(text, str):
+    """Returns a text with new line after special chars
+
+    Args:
+        text
+    """
+    if (not isinstance(text, str)):
         raise TypeError('text must be a string')
     i = 0
-    while i < len(text):
-        print(text[i], end='')
-        if text[i] in ['.', '?', ':']:
-            print()
-            print()
-        i += 1
+    for char in text:
+        if i == 0:
+            if char == ' ':
+                continue
+            else:
+                i = 1
+        if i == 1:
+            if char in ".?:":
+                print(char)
+                print()
+                i = 0
+            else:
+                print(char, end="")
