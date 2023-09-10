dea1879f5f7c0b26cb79b007e8ca2ec814317b97
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-10 12:12:31 -0700

    task 4

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A function that prints"""
+
+
+def print_square(size):
+    """Prints a square with character #"""
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif (type(size) == float) and (size < 0):
+        raise TypeError("size must be an integer")
+    elif size == 0:
+        pass
+    else:
+        for i in range(size ** 2):
+            if i and not(i % size):
+                print()
+            print(f'#', end="")
+        print()
