debb921bffe639f3622c79aff3101f8a2b63cb57
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-11 21:12:37 -0700

    a class basegeometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""a class BaseGeometry with public instance"""
+
+
+class BaseGeometry:
+    """a class with public instance"""
+    def area(self):
+        raise Exception('area() is not implemented')
