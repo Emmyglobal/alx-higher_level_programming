bc36351dc7f2accf85b1462426cccf60db0fc438
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-20 07:46:49 -0700

    hot fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,8 +6,19 @@ A class that inherits from Base class with other methods
 
 
 class Rectangle(Base):
-    """The first constructor of the class """
+    """
+    The first constructor of the class
+    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        The first constructor of the rectangle class.
+        Args:
+            Width: This variable holds the width of a rectangle.
+            height: This variable holds the height of a rectangle
+            x: x-axis of the rectangle
+            y: y-axis of the rectangle
+            id: this variable holds the id of the instance.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,6 +27,10 @@ class Rectangle(Base):
 
         @property
         def width(self):
+            """
+            This is the getter of the width variable
+            this method returns the width of the rectangle.
+            """
             return self.__width
 
         @width.setter
@@ -28,6 +43,9 @@ class Rectangle(Base):
 
         @property
         def height(self):
+            """
+            This method returns the height of the rectangle.
+            """
             return self.__height
 
         @height.setter
@@ -40,6 +58,9 @@ class Rectangle(Base):
 
         @property
         def x(self):
+            """
+            This is the getter for the x-axis
+            """
             return self.__x
 
         @x.setter
@@ -52,6 +73,9 @@ class Rectangle(Base):
 
         @property
         def y(self):
+            """
+            This is the method for the y-axis
+            """
             return self.__y
 
         @y.setter
