cf240301c4f793ab49e766266a5d7d11749c5885
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-26 08:24:44 -0700

    print out

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""a class Rectangle that defines a rectangle:"""
+
+
+class Rectangle:
+    """
+    A class Rectangle that defines a rectangle by: (based on 0-rectangle.py)
+    """
+    def __init__(self, width=0, height=0):
+        """
+        The init method
+        Args:
+            width: the width of the rectangle
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Getter for the width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Setter for the width
+        Args:
+            value: The values of the width
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        returns the area of the rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        Returns the perimeter of a rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rectangle_str = ""
+        for i in range(self.__height):
+            rectangle_str += "#" * self.__width + "\n"
+        return rectangle_str.rstrip()
