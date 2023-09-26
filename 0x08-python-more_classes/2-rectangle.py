7d5a409208faaa589b01a7e703ee362d6e46f5ce
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-09-26 07:28:21 -0700

    area fixed

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -48,7 +48,15 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        return width * heigth
+        """
+        returns the area of the rectangle
+        """
+        return self.__width * self.__heigth
 
     def perimeter(self):
-        return width + height
+        """
+        Returns the perimeter of a rectangle
+        """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2(self.__width + self.__height)
