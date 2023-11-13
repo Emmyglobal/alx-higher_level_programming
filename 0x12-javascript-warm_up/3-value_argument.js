0b5c65a127ea44b3b5f72aece42a2fc43544cff1
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-11-13 01:55:41 -0800

    Print args

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -3,10 +3,8 @@
 
 const args = process.argv;
 const i = args.length;
-if (i <= 2) {
+if (i === 2) {
   console.log('No argument');
-} else if (i === 3) {
-  console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log(args[2]);
 }
