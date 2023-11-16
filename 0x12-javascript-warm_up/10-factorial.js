9923d8fd8d6bb17fcfadb9defef28b224d91a6db
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-11-16 04:39:00 -0800

    factorial fixed

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,10 +4,10 @@
 const args = process.argv;
 const input = parseInt(args[2]);
 console.log(fact(input));
-function fact(n) {
+function fact (n) {
   if (n === 0 || isNaN(n)) {
-	 return 1;
+    return 1;
   } else {
     return n * fact(n - 1);
-}
+  }
 }
