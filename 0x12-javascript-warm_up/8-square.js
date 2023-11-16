628256512941b5fa1210e1ea85e8bcd88f20cfd2
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-11-16 03:10:02 -0800

    Print square fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -6,7 +6,7 @@ const num = parseInt(args[2]);
 if (!num) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i <= num; i++) {
+  for (let i = 1; i <= num; i++) {
     console.log('X'.repeat(num));
   }
 }
