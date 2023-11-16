4686b844c1d95b664d70463ff2d7363503919546
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-11-16 04:04:55 -0800

    add fixed semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -5,6 +5,6 @@ const args = process.argv;
 const arg1 = parseInt(args[2]);
 const arg2 = parseInt(args[3]);
 console.log(add(arg1, arg2));
-function add(a, b) {
+function add (a, b) {
   return a + b;
 }
