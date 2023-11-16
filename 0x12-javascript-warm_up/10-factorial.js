5dffcc014ce05ca7bbdbaa5cb4a5d482f950a15c
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-11-16 04:28:22 -0800

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+/* Computes and prints a factorial */
+
+const args = process.argv;
+const input = parseInt(args[2]);
+console.log(fact(input));
+function fact(n) {
+  if (n <= 0 || isNaN(n)) {
+	 return 1;
+  } else {
+    return n * fact(n - 1);
+}
+}
