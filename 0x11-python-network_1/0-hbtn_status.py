0a428469560054569b41b03580993961570749af
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-12-02 13:26:30 -0800

    fetch data fixed

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -9,9 +9,11 @@ url = "https://alx-intranet.hbtn.io/status"
 
 try:
     with urllib.request.urlopen(url) as response:
-        content = response.read().decode("utf-8")
+        content = response.read()
+        content_str = content.decode("utf-8")
         print("Body response:")
         print("\t- type:", type(content))
         print("\t- content:", content)
+        print("\t- utf8 content:", content_str)
 except urllib.error.URLError as e:
     print(f"Failed to fetch data. Error: {e}")
