e0d1206b939f2e2d90b8ed7b96f56fa37c1b9d8b
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-12-02 14:53:24 -0800

    wit email

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,13 +4,8 @@ script that takes in a URL, sends a request to the URL
 and displays the value of the X-Request-Id variable 
 found in the header of the response.
 """
-
-import sys
 import urllib.request
-
-if len(sys.argv) != 2:
-    print("Usage: python script.py <URL>")
-    sys.exit(1)
+import sys
 
 url = sys.argv[1]
 
---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email, Send a post request to the passed URL
+with the email as a parameter and displays the body of the response
+"""
+
+import urllib.request
+import urllib.parse
+import sys
+
+url = sys.argv[1]
+email = sys.argv[2]
+
+# Encode the emai; parameter
+data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+try:
+    with urllib.request.urlopen(url, data=data) as response:
+        decoded_response = response.read().decode('utf-8')
+        print(decoded_response)
+except urllib.error.HTTPError as e:
+    print(f"HTTPError: {e.code} - {e.reason}")
+except urllib.error.URLError as e:
+    print(f"Failed to fetch data. Error: {e}")
