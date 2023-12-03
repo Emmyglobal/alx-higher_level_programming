4853366667ff2c1b27e78ba8925cd7c926a94efd
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-12-03 10:26:03 -0800

    Still fixing

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,11 +4,11 @@ script that takes in a URL, sends a request to the URL
 and displays the value of the X-Request-Id variable
 found in the header of the response.
 """
-
-
 import urllib.request
 import sys
-f __name__ == "__main__":
+
+
+if __name__ == "__main__":
     url = sys.argv[1]
     try:
         with urllib.request.urlopen(url) as response:
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,6 +9,8 @@ the body of the response
 import urllib.request
 import urllib.parse
 import sys
+
+
 if __name__ = "__main__":
     url = sys.argv[1]
     email = sys.argv[2]
