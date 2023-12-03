06ad8f04fb0f726da74f3c8ba699cc15b97aa3d5
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-12-03 07:47:09 -0800

    decoded request

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL
+Sends request to the URL and
+Displays the body of the response(decode in utf-8)'
+"""
+import urllib.request
+import urllib.error
+import sys
+
+
+url = sys.argv[1]
+
+try:
+    with urllib.request.urlopen(url) as response:
+        decoded_response = response.read().decode('utf-8')
+        print(decoded_response)
+except urllib.error.HTTPError as e:
+    print(f"Error code: {e.code}")
+except urllib.error.URLError as e:
+    print(f"Failed to fetch data. Error: {e}")
