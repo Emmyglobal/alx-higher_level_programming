35421fd90d9e42fed993089b3761b6555056a522
Emmyglobal <nwaforugochukwu21@gmail.com>
2023-12-03 10:40:42 -0800

    Still fixing

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     try:
         with urllib.request.urlopen(url) as response:
             if 'X-Request-Id' in response.headers:
-                request_id = response.headers['X-Request-Id']
+                request_id = dict(response.headers).get('X-Request-Id')
                 print(request_id)
             else:
                 print("X-Request-Id not found in the response headers.")
