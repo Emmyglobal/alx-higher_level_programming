55ef639bb0642f2e33470c8198fbf2e59d750d91
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-14 21:45:28 +0100

    cutting out a letter, corrected python codestyle validation

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    ans = ""
-    if n >= 0 and n <= len(str):
-        for i in range(len(str) - 1):
-            if i == n:
-                str = str.replace(str[i], "")
-            ans += str[i]
-        return ans
-    else:
+    if n < 0 or n >= len(str):
         return str
+    return str[:n] + str[(n + 1):]
