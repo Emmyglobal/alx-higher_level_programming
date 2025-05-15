02ece6048219cca4044881089ff1ea785356f1f0
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-15 15:59:55 +0100

    importing module commenting

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,6 @@
 #!/usr/bin/python3
-import sys
-"""My addition function
-
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-"""
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add 
     a = 1
     b = 2
-    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
