dac6f414891ad6cf1c812ad3125b52bd55144fa7
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-15 15:48:19 +0100

    importing module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import sys
+"""import modules in python"""
 if __name__ == "__main__":
     import add_0
-
     a = 1
     b = 2
     print("{} + {} = {}".format(a, b, add_0.add(1, 2)))
