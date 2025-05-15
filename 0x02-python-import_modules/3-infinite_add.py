c808126d56c1aa174acb3add263ce58d864f216c
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-15 12:21:12 +0100

    importing module

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-if __name__ == '__main__':
-    import sys
-    num_par = len(sys.argv) - 1
-    sum = 0
-    for i in range(num_par):
-        sum += int(sys.argv[i + 1])
-    print("{}".format(sum))
