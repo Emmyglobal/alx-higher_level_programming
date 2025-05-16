dc37b2e1cf39d4f0e960f5d89fdc7e97211cc8bf
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-16 12:52:09 +0100

    sum argument

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+if __name__ == "__main__":
+    arg = sys.argv
+    count = len(sys.argv)
+    sums = 0
+    for i in range(1, count):
+       sums += int(arg[i])
+
+    print(sums)
