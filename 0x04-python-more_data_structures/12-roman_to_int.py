5bdff9670b7190c9915c79bb97ff3948236d609f
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-25 10:42:18 +0100

    Technical interview preparation, error fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-from functools import reduce
 def roman_to_int(roman_string):
-    letters = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     listOfNum = []
     sumTheList = 0
     for i in roman_string:
-            for k, v in letters.items():
-                if i == k:
-                     listOfNum.append(v)
+        for k, v in letters.items():
+            if i == k:
+                listOfNum.append(v)
     for i in range(len(listOfNum) - 1):
         if listOfNum[i] < listOfNum[i + 1]:
             sumTheList -= listOfNum[i]
