b90ed1e85250be7545285c1d3bf17a99dfefdde7
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-25 11:28:09 +0100

    Technical interview preparation, error fixed

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+    multiplyAndSum = 0
+    denominator = 0
+    for i in my_list:
+        x, y = i
+        multiplyAndSum += (x * y)
+        denominator += y
+    res = multiplyAndSum / denominator
+    return res
+        
