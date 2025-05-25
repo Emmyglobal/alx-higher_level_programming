7517e38def889bb1676260e80aaeb60300d4313f
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-05-25 10:55:13 +0100

    Technical interview preparation, error fixed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     letters = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     listOfNum = []
     sumTheList = 0
-    if not roman_string:
-        return None
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
     for i in roman_string:
         for k, v in letters.items():
             if i == k:
