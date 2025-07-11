26d3a3295434f61fc8ec473ea51931a876312ae2
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-07-11 10:21:18 +0100

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,7 @@
 """A function that reads a text file with utf-8 encoding"""
 
 def read_file(filename=""):
+    """Open and read a file(UTF8)and prints it to stdout"""
     with open(filename, mode='r', encoding="utf-8") as f:
         text = f.read()
     print(text, end="")
