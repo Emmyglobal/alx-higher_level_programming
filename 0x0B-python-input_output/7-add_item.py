e54987ef266e4198e87dd4f14ae3b832df50f322
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-07-11 19:59:26 +0100

    add command line

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Adds all arguments to a python list, and then save them to a file"""
+import sys
+save2json = __import__("5-save_to_json_file").save_to_json_file
+load4rm = __import__("6-load_from_json_file").load_from_json_file
+
+my_list = load4rm("add_item.json")
+my_list.extend(sys.argv[1:])
+save2json(my_list, "add_item.json")
