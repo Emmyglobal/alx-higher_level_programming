a3cfead0aa08f6da0b0123bba04283cb1b780d96
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-07-11 20:02:38 +0100

    add command line

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,10 @@ import sys
 save2json = __import__("5-save_to_json_file").save_to_json_file
 load4rm = __import__("6-load_from_json_file").load_from_json_file
 
-my_list = load4rm("add_item.json")
+try:
+    my_list = load4rm("add_item.json")
+except FileNotFoundError:
+    my_list = []
+
 my_list.extend(sys.argv[1:])
 save2json(my_list, "add_item.json")
