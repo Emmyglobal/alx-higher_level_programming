85a034387e50ba503243deb91dc163a9e98c1840
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-07-11 11:21:13 +0100

    length of written text

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,4 +4,5 @@
 
 def write_file(filename="", text=""):
     """returns the number of character written"""
-    with open("filename", "")
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
