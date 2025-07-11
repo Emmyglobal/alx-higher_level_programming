b201ab308c683bbc5dddc1d3f051239ea06f3909
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-07-11 10:33:29 +0100

    fixed

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@
 
 def read_file(filename=""):
     """Open and read a file(UTF8)and prints it to stdout"""
-    with open(filename, mode='r', encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         text = f.read()
     print(text, end="")
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A function that writes a string to a text file(UTF8)"""
+
+
+def write_file(filename="", text=""):
+    """returns the number of character written"""
+    with open("filename", "")
