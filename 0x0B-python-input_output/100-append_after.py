7e0746ea00753bd7e85e982c2338a91206570ebb
Nwafor Ugochukwu <nwaforugochukwu21@gmail.com>
2025-07-12 21:56:46 +0100

    add content

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""function that inserts a line of text to a file after each
+    after each line containing a specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, "r", encoding="utf-8") as f:
+        a = f.readlines()
+
+    my_word_list = []
+    for i in a:
+        my_word_list.append(i)
+        
+        if search_string in i:
+            my_word_list.append(new_string)
+
+    print(my_word_list)
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.writelines(my_word_list)
